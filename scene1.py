60697fff81262f2324e0428dfca8a72b7909ff0f
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-06-07 10:10:58 +0200

    Better comments in the 1st scene written in Vapory

--- scene1.py
+++ scene1.py
@@ -15,17 +15,22 @@
 
 from vapory import *
 
+# často používané vektory
+x = [1, 0, 0]
 y = [0, 1, 0]
+z = [0, 0, 1]
 
-
+# vytvoření kamery ve scéně
 camera = Camera('location', [0.0, 2.0, -3.0],
                 'up',       [0.0, 1.0, 0.0],
                 'right',    [4/3, 0.0, 0.0],
                 'look_at',  [0.0, 1.0, 2.0])
 
+# světelný zdroj s bílou barvou
 light = LightSource([2, 4, -3],
                     'color', [1, 1, 1])
 
+# trojrozměrný objekt - koule
 sphere = Sphere([0, 1, 2],
                 2.0,
                 Texture(
@@ -34,13 +39,16 @@ sphere = Sphere([0, 1, 2],
                            'phong_size', 300,
                            'reflection', 0.15)))
 
+# trojrozměrný objekt - nekonečná rovina
 plane = Plane(y,
               -1.5,
               Texture(
                   Pigment('color', [1, 1, 1]),
                   Finish('reflection', 0.10)))
 
+# složení scény z jednotlivých objektů
 scene = Scene(camera,
               objects=[light, sphere, plane])
 
+# vykreslení scény do souboru
 scene.render("scene1_vapory.png", width=640, height=480)
