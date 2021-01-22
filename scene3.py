c049ad5fb70631ab9f5b14b1be099e012b1dd408
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-22 10:17:50 +0100

    Fixed issue #9

--- scene3.py
+++ scene3.py
@@ -24,7 +24,7 @@ z = [0, 0, 1]
 camera = Camera('location', [1.65, 5.5, -5.0],
                 'up',       [0.0, 1.0, 0.0],
                 'right',    [4/3, 0.0, 0.0],
-                'look_at',  [ 0.0, 0.5,-1.0])
+                'look_at',  [0.0, 0.5,-1.0])
 
 # tři světelné zdroje
 light1 = LightSource([-30, 11, 20],
