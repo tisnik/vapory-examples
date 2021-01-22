a8f86813de3731b53df1e085b74f94892c8db7c3
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-01-22 10:11:27 +0100

    Fixed issue #2

--- scene2_update.py
+++ scene2_update.py
@@ -22,7 +22,7 @@ z = [0, 0, 1]
 
 # vytvoření kamery ve scéně
 camera = Camera('location', [1.65, 5.5, -5.0],
-                'up',       [ 0.0, 1.0, 0.0],
+                'up',       [0.0, 1.0, 0.0],
                 'right',    [ 4/3, 0.0, 0.0],
                 'look_at',  [ 0.0, 0.5,-1.0])
 
